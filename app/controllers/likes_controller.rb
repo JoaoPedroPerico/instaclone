0bc5a29ad48fbfc4b39aab58b68e63385ad28752
João Pedro Périco <joaopedroperico@gmail.com>
2023-09-05 11:06:05 -0300

    Typos

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,15 @@
 class LikesController < ApplicationController
     def create
-        if  params[:likeable_type] == 'Post'
+        if  params[:likeable_type] == "Post"
             @likeable = Post.find(params[:post_id])
             current_user.like!(@likeable)
         end
     end
 
     def destroy
-        if  params[:likeable_type] == 'Post'
+        if  params[:likeable_type] == "Post"
             @likeable = Post.find(params[:post_id])
-            Like.where(likeable_type: 'Post', likeable_id: @likeable.id, user_id: current_user.id).delete_all
+            Like.where(likeable_type: "Post", likeable_id: @likeable.id, user_id: current_user.id).delete_all
         end
     end
 end
