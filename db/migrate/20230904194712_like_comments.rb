868345dac9d2fb4cb5869d28cf4a235ae68cf796
João Pedro Périco <joaopedroperico@gmail.com>
2023-09-06 09:03:31 -0300

    Typo adjust on migrations

--- db/migrate/20230904194712_like_comments.rb
+++ db/migrate/20230904194712_like_comments.rb
@@ -1,7 +1,7 @@
 class LikeComments < ActiveRecord::Migration[7.0]
   def change
     change_table :likes do |t|
-      table.remove :post_id
+      t.remove :post_id
       t.references :likeable, polymorphic: true, null: false
     end
   end
